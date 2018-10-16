153c2586265834ad4da76c152a9c31fdc6e24417
fruit <884836982@qq.com>
2018-10-16 18:38:33 +0800

    第5次提交

--- script/main.js
+++ script/main.js
@@ -28,19 +28,19 @@ $(function() {
         });
     });
     $(".city_name").mouseout(function() {
-            $(this).css({
-                background: "none",
-                color: "#008842"
-            });
-            $(".city-con").css({
-                display: "none"
-            });
-            $(".city_name span").css({
-                background: "url(../images/header-img.png) no-repeat 0 0",
-                transform: "rotate(0deg)"
-            });
-        })
-        //二级菜单显示隐藏
+        $(this).css({
+            background: "none",
+            color: "#008842"
+        });
+        $(".city-con").css({
+            display: "none"
+        });
+        $(".city_name span").css({
+            background: "url(../images/header-img.png) no-repeat 0 0",
+            transform: "rotate(0deg)"
+        });
+    });
+    //二级菜单显示隐藏
     $(".city-con").mouseenter(function() {
         $(this).css({
             display: "block"
@@ -205,11 +205,229 @@ $(function() {
     })
 })
 $(".b-bot li").mouseleave(function() {
-    $(this).removeClass("on")
-        .siblings("li").removeClass("on");
-    $(".b-img li").eq($(this).index()).css({
-        display: "block"
-    }).siblings("li").css({
-        display: "none"
+        $(this).removeClass("on")
+            .siblings("li").removeClass("on");
+        $(".b-img li").eq($(this).index()).css({
+            display: "block"
+        }).siblings("li").css({
+            display: "none"
+        })
     })
+    //加入购物车
+$(".product_item").mouseenter(function() {
+    $(this).addClass("current").css({
+
+            // -webkit - transform:"translateX(5px)",
+            // -moz - transform: "translateX(5px)",
+            // -ms - transform: "translateX(5px)",
+            // -o - transform: "translateX(5px)",
+            transform: "translateX(5px)",
+        })
+        .siblings(".product_item").removeClass("current").css({
+            transform: "translateX(0px)"
+        })
+});
+//回到顶部
+$(".goTop").click(function() {
+    $("html,body").scrollTop(0);
+});
+//吸顶菜单
+$(document).scroll(function() {
+        var scrollTop = $("html,body").scrollTop();
+        if (scrollTop >= 100) {
+            $("#search").addClass("search_fixed")
+        } else {
+            $("#search").removeClass("search_fixed")
+        }
+        if (scrollTop >= 500) {
+            $(".goTop").css({
+                display: "block"
+            })
+        } else {
+            $(".goTop").css({
+                display: "none"
+            })
+        }
+    })
+    //购物车
+function ShopCar() {}
+$.extend(ShopCar.prototype, {
+    init: function() {
+        this.main = $("#goods ul");
+        this.timer = null;
+        this.loadJson()
+            .then(function(res) {
+                this.json = res.goodsList;
+                console.log(res);
+                console.log(this.json);
+                this.renderPage()
+            })
+        this.bindEvent();
+        this.listSum();
+    },
+    loadJson: function() {
+        var opt = {
+            url: "http://localhost:8888/proxy/script/data.json",
+            type: "GET",
+            context: this
+        }
+        return $.ajax(opt);
+    },
+    renderPage: function() {
+        console.log(this.json);
+        var html = "";
+        for (var i = 0; i < this.json.length; i++) {
+            html += ` <li class="product_item j_product current">
+            <div class="p_img clearfix">
+                <a href="javascript:;" target="_blank">
+                    <img src="${this.json[i].img}" width="290" height="290" class="j_product_img">
+                </a>
+            </div>
+            <div class="p_info clearfix">
+                <div class="p_name">
+                    <a href="javascript:;" target="_blank">
+                    ${this.json[i].title}
+                        <font color="red">榴莲</font>蜜语
+                        <font color="red">榴莲</font>班戟150g(6个)</a>
+                </div>
+                <div class="p_price">
+                    <span class="price"><strong>¥${this.json[i].price}</strong></span>
+
+                </div>
+            </div>
+
+            <div class="p-buy">
+                <span></span>
+                <a class="btn-buy" href="javascript:;" data-id=${this.json[i].id}>加入购物车</a>
+            </div>
+        </li> `
+        }
+        this.main.html(html);
+    },
+    bindEvent: function() {
+        $("#goods ul").on("click", ".btn-buy", this.addCar.bind(this));
+        $(".shopping-cart").on("mouseenter", this.showList.bind(this));
+        $(".shopping-btn").on("mouseenter", function() {
+            clearTimeout(this.timer)
+            $(".shopping-list").css({
+                display: "block"
+            })
+        })
+        
+        // $(".shopping-btn").on("mouseleave", function() {
+        //     clearTimeout(this.timer)
+        //     this.timer = setTimeout(function(){
+        //         $(".shopping-list").css({
+        //             display: "none"
+        //         })
+        //     },500)
+        // });
+        $(".shopping-list").on("mouseenter", function() {
+            clearTimeout(this.timer)
+            $(this).css({
+                display: "block"
+            })
+        })
+        $(".shopping-list").on("mouseleave", function() {
+            clearTimeout(this.timer)
+            $(this).css({
+                display: "none"
+            })
+        })
+        $(".shopping-cart").on("click", function(event) {
+            var target = event.target;
+            if (target != $(".shopping-cart")[0]) return 0;
+            $.removeCookie("shopCar");
+            $(".shopping-list").triggerHandler("mouseleave");
+            this.listSum();
+        }.bind(this))
+    },
+    addCar: function(event) {
+        var target = event.target;
+        var goodsId = $(target).attr("data-id");
+        var cookie;
+        if ((cookie = $.cookie("shopCar"))) {
+            var cookieArray = JSON.parse(cookie);
+            var hasGoods = false;
+            for (var i = 0; i < cookieArray.length; i++) {
+                if (cookieArray[i].id == goodsId) {
+                    hasGoods = true;
+                    cookieArray[i].num++;
+                    break;
+                }
+            }
+            if (hasGoods == false) {
+                var goods = {
+                    id: goodsId,
+                    num: "1"
+                }
+                cookieArray.push(goods);
+            }
+            $.cookie("shopCar", JSON.stringify(cookieArray));
+        } else {
+            $.cookie("shopCar", `[{"id":"${goodsId}","num":"1"}]`);
+        }
+        console.log($.cookie("shopCar"));
+        this.listSum();
+    },
+
+    showList: function() {
+        // var target = event.target;
+
+        // if (target != $(".shopping-cart dl")[0]) return 0;
+        var cooke;
+        if (!(cookie = $.cookie("shopCar"))) {
+            return 0;
+        };
+        var cookieArray = JSON.parse(cookie);
+        console.log(cookieArray);
+        var html = "";
+        for (var i = 0; i < cookieArray.length; i++) {
+            for (var j = 0; j < this.json.length; j++) {
+                console.log(this.json[j].id, cookieArray[i].id);
+                if (cookieArray[i].id == this.json[j].id) {
+                    html += `<li data-id = ${cookieArray[i].id}><div class="l"><a href="http://www.yiguo.com/product/1356627.html" target="_blank">
+                    <img src="${this.json[j].img}" width="42" height="42"></a></div><div class="c">
+                <a href="http://www.yiguo.com/product/1356627.html">${this.json[j].title}</a></div><div class="r"><b>¥${this.json[j].price}</b> * ${cookieArray[i].num}<a href="javascript:;" onclick="remove(${cookieArray[i].id})">删除</a></div></li>`;
+                    break;
+                }
+            }
+        }
+        console.log(html);
+        $(".shopping-list .nogoods").css({
+            display: "none"
+        })
+        $(".shopping-list .goods ul").html(html);
+    },
+    listSum: function() {
+        var cookie;
+        if (!(cookie = $.cookie("shopCar"))) {
+            $(".shopping-cart .totleNum").find("b").html(0);
+            return 0;
+        }
+        var cookieArray = JSON.parse(cookie);
+        var sum = 0;
+        var totlePrice = 0
+        for (var i = 0; i < cookieArray.length; i++) {
+            console.log(cookieArray)
+            sum += Number(cookieArray[i].num);
+            for (var j = 0; j < this.json.length; j++) {
+                if (cookieArray[i].id == this.json[j].id) {
+                    totlePrice += Number(cookieArray[i].num) * Number(this.json[j].price);
+                }
+            }
+
+        }
+        console.log(totlePrice);
+        $(".shopping-cart .totleNum").find("b").html(sum);
+        $(".shopping-cart .totlePrice").html(totlePrice);
+    },
+    
 })
+var shopcar = new ShopCar();
+shopcar.init();
+function remove(id){
+    $.removeCookie("id",{path:"/"});
+    
+    shopcar.showList();
+ }
